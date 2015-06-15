681fb61724d5ef57b256d4a960ad833218d21409
Coonax <guillaume.gaudenot@viacesi.fr>
2015-06-15 13:59:53 +0200

    Navigation : MainActivity -> ActivitiesActivity.

--- app/src/main/java/com/example/coonax/coonax/MainActivity.java
+++ app/src/main/java/com/example/coonax/coonax/MainActivity.java
@@ -1,18 +1,35 @@
 package com.example.coonax.coonax;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.coonax.coonax.ui.ActivitiesActivity;
 
 
 public class MainActivity extends Activity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button switchButtonActivities = (Button) findViewById(R.id.button_activities);
+
+
+        switchButtonActivities.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ActivitiesActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
