ee2e7f82b18e26be4d73de1a818a0301efdd61d5
JigSawFr <sebastien.robert@viacesi.fr>
2015-06-15 15:53:09 +0200

    Service PuyDuFou (Interface) Test sur MainActivity

--- app/src/main/java/com/example/coonax/coonax/MainActivity.java
+++ app/src/main/java/com/example/coonax/coonax/MainActivity.java
@@ -2,8 +2,19 @@ package com.example.coonax.coonax;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+import com.example.coonax.coonax.model.Shop;
+import com.example.coonax.coonax.service.*;
+import retrofit.Callback;
+import retrofit.RestAdapter;
+import retrofit.RetrofitError;
+import retrofit.android.AndroidLog;
+import retrofit.client.Response;
+
+import java.util.List;
 
 
 public class MainActivity extends Activity {
@@ -12,6 +23,33 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Toast.makeText(getApplicationContext(), "TEST", Toast.LENGTH_SHORT).show();
+        PuyDuFou puyDuFouService = new RestAdapter.Builder()
+                .setEndpoint(PuyDuFou.ENDPOINT)
+                .setLog(new AndroidLog("retrofit"))
+                .setLogLevel(RestAdapter.LogLevel.FULL)
+                .build()
+                .create(PuyDuFou.class);
+
+       puyDuFouService.listShopAsync(new Callback<List<Shop>>() {
+            @Override
+            public void success(List<Shop> shops, Response response) {
+                Log.d("RESTFULL", "NICE ON A LES BOUTIQUES !!!!");
+                afficherShops(shops);
+            }
+
+           @Override
+            public void failure(RetrofitError error) {
+               Toast.makeText(getApplicationContext(), "FAIL" + error, Toast.LENGTH_SHORT).show();
+               Log.w("RESTFULL", "FAIL: " + error);
+            }
+        });
+    }
+
+    public void afficherShops(List<Shop> shops) {
+        //Toast.makeText(this, "Nb boutiques : " + shops.size(), Toast.LENGTH_SHORT).show();
+        Log.d("RESTFULL", "On a " + shops.size() + " boutiques chez PuyDuOUF !");
     }
 
     @Override
--- app/src/main/java/com/example/coonax/coonax/service/PuyDuFou.java
+++ app/src/main/java/com/example/coonax/coonax/service/PuyDuFou.java
@@ -7,32 +7,25 @@ import retrofit.Callback;
 import retrofit.http.GET;
 import retrofit.http.Path;
 
-public class PuyDuFou {
+public interface PuyDuFou {
 
-    private static final String DOMAIN = "10.154.128.142";
-    private static final String PORT = "100800";
-    private static final String PATH = "/puyDuFou/webresources";
-    //public static final String ENDPOINT = "http://10.154.128.142:10080/puyDuFou/webresources";
-
-    public String getENDPOINT() {
-        return "http://" + DOMAIN + ":" + PORT + PATH;
-    }
+    String ENDPOINT = "http://10.154.128.142:10080/puyDuFou/webresources";
 
     @GET("/com.puydufou.entity.shops")
-    void listShopAsync(Callback<List<Shop>> callback) {}
+    void listShopAsync(Callback<List<Shop>> callback);
 
     @GET("/com.puydufou.entity.shops/{id}")
-    void shopAsync(@Path("id") int id, Callback<Shop> callback) {}
+    void shopAsync(@Path("id") int id, Callback<Shop> callback);
 
     @GET("/com.puydufou.entity.shows")
-    void listShowAsync(Callback<List<Show>> callback) {}
+    void listShowAsync(Callback<List<Show>> callback);
 
     @GET("/com.puydufou.entity.shows/{id}")
-    void showAsync(@Path("id") int id, Callback<Shop> callback) {}
+    void showAsync(@Path("id") int id, Callback<Shop> callback);
 
     @GET("/com.puydufou.entity.restaurants")
-    void listRestaurantAsync(Callback<List<Restaurant>> callback) {}
+    void listRestaurantAsync(Callback<List<Restaurant>> callback);
 
     @GET("/com.puydufou.entity.restaurants/{id}")
-    void restaurantAsync(@Path("id") int id, Callback<Restaurant> callback) {}
+    void restaurantAsync(@Path("id") int id, Callback<Restaurant> callback);
 }
