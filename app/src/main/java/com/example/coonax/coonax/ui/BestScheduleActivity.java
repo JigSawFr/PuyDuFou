ce7cb34358aafdf283f18ebb5b4989a5448145d7
Coonax <guillaume.gaudenot@viacesi.fr>
2015-06-16 13:55:32 +0200

    Création BestScheduleActivity (+ navigation)

--- app/src/main/java/com/example/coonax/coonax/ui/BestScheduleActivity.java
+++ app/src/main/java/com/example/coonax/coonax/ui/BestScheduleActivity.java
@@ -2,45 +2,32 @@ package com.example.coonax.coonax.ui;
 
 import android.app.ActionBar;
 import android.app.Activity;
-import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 import com.example.coonax.coonax.R;
 
-public class ScheduleActivity extends Activity {
+public class BestScheduleActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_schedule);
+        setContentView(R.layout.activity_best_schedule);
 
         //Affichage de la ActionBar coloré
         ActionBar ab = getActionBar();
         ColorDrawable colorDrawable = new ColorDrawable(Color.parseColor("#3498db"));
         ab.setBackgroundDrawable(colorDrawable);
 
-        //Bouton d'accès à l'activité BestScheduleActivity
-
-        Button switchButtonBestSchedule = (Button) findViewById(R.id.button_best_schedule);
-        switchButtonBestSchedule.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(ScheduleActivity.this, BestScheduleActivity.class);
-                startActivity(intent);
-            }
-        });
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_schedule, menu);
+        getMenuInflater().inflate(R.menu.menu_best_schedule, menu);
         return true;
     }
 
