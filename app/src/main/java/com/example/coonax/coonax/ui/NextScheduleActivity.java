82caef0f5b814d581b87b5418f4c8e9f5b0f65fc
JigSawFr <sebastien.robert@viacesi.fr>
2015-06-18 17:19:13 +0200

    NextActivity Added

--- app/src/main/java/com/example/coonax/coonax/ui/NextScheduleActivity.java
+++ app/src/main/java/com/example/coonax/coonax/ui/NextScheduleActivity.java
@@ -5,9 +5,36 @@ import android.app.Activity;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 import com.example.coonax.coonax.R;
+import com.example.coonax.coonax.model.Schedule;
+import com.example.coonax.coonax.service.PuyDuFou;
+import retrofit.Callback;
+import retrofit.RestAdapter;
+import retrofit.RetrofitError;
+import retrofit.android.AndroidLog;
+import retrofit.client.Response;
+
+import java.util.List;
+
+import static com.example.coonax.coonax.adapter.ScheduleAdapter.formatShowTime;
+
+/**
+ * Projet       ~~ PuyDuFou ~~
+ ****************************************
+ * Créé par JigSaw le 17/06/2015 à 02:45
+ ****************************************
+ *        ___ ______     ___ _       __
+ *       / (_) ____/____/   | |     / /
+ *  __  / / / / __/ ___/ /| | | /| / /
+ * / /_/ / / /_/ (__  ) ___ | |/ |/ /
+ * \____/_/\____/____/_/  |_|__/|__/
+ *
+ */
 
 public class NextScheduleActivity extends Activity {
 
@@ -16,10 +43,50 @@ public class NextScheduleActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_next_schedule);
 
-        //Affichage de la ActionBar coloré
+        /* ~~ COONAX ## START ~~ */
         ActionBar ab = getActionBar();
         ColorDrawable colorDrawable = new ColorDrawable(Color.parseColor("#16a085"));
         ab.setBackgroundDrawable(colorDrawable);
+        /* ~~ COONAX ## END ~~ */
+
+        Log.i("PUYDUFOU", "SHOW_NEXT_ACTIVITY :: Demande d'informations sur le la prochaine activité");
+        refreshNextActivity();
+    }
+
+    private void refreshNextActivity() {
+        PuyDuFou puyDuFouService = new RestAdapter.Builder()
+                .setEndpoint(PuyDuFou.ENDPOINT)
+                .setLog(new AndroidLog("retrofit"))
+                .setLogLevel(RestAdapter.LogLevel.FULL)
+                .build()
+                .create(PuyDuFou.class);
+        try {
+            puyDuFouService.listOptimizedScheduleAsync("09", "00", "30", new Callback<List<Schedule>>() {
+                @Override
+                public void success(List<Schedule> myNextActivities, Response response) {
+                    Schedule myNextActivity = myNextActivities.get(0);
+                    Log.i("PUYDUFOU", "SHOW_NEXT_ACTIVITY :: La prochaine activité a été réceptionnée avec succès !");
+
+                    TextView name = (TextView) findViewById(R.id.text_next_activity);
+                    TextView desc = (TextView) findViewById(R.id.desc_next_activity);
+                    TextView time = (TextView) findViewById(R.id.time_next_activity);
+
+                    name.setText(myNextActivity.getShow().getName());
+                    desc.setText(myNextActivity.getShow().getLongDescription());
+                    time.setText(formatShowTime(myNextActivity.getStartTime(), "HH:mm"));
+                }
+
+                @Override
+                public void failure(RetrofitError error) {
+                    Log.w("PUYDUFOU", "SHOW_NEXT_ACTIVITY :: Impossible de récupérer la prochaine activité");
+                    Log.w("PUYDUFOU", "SHOW_NEXT_ACTIVITY :: " + error);
+                    Toast.makeText(getApplicationContext(), "FAIL" + error, Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+        catch (Exception e) {
+            Log.w("PUYDUFOU", "SHOW_NEXT_ACTIVITY :: EXCEPTION: " + e);
+        }
     }
 
 
